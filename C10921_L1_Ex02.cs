f574628b724935e287629678e306b3e5a8bbbd50
Roberto Ferreira <rhsantanaferreira@gmail.com>
2026-04-19 06:18:52 -0300

    Calculo de área e perímetro
    2. Faça o algoritmo (diagrama de blocos e português estruturado) para calcular a área (AREA) e o perímetro (P) de uma sala, sendo que os comprimentos (L e C) são fornecidos pelo usuário. Apresente a área e o perímetro depois de calculados.

---
+++ C10921_L1_Ex02.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj10921_L1_EX02
+{
+    internal class C10921_L1_EX02
+    {
+        static void Main(string[] args)
+        {
+
+            //Exercício 02.
+
+            //Área da sala e perímetro.
+
+            #region
+
+            double L, C, AREA, P;
+
+            Console.Write("Digite aqui a largura da sala: ");
+            L = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite aqui o comprimento da sala: ");
+            C = Convert.ToDouble(Console.ReadLine());
+
+            AREA = L * C;
+
+            P = 2 * (L + C);
+
+            Console.WriteLine("A área da sala é: " + AREA + "m²");
+
+            Console.WriteLine("O perímetro da sala é: " + P + "m");
+
+            Console.ReadKey();
+
+            #endregion
+
+        }
+    }
+}
