8ad851dc3d89cdd3bd4614ff1e40e26f9f6cfbef
Roberto Ferreira <rhsantanaferreira@gmail.com>
2026-04-19 06:29:35 -0300

    Cáculo de desconto
    Faça o algoritmo (diagrama de blocos e português estruturado) para calcular o valor monetário do desconto (D), sendo que o preço do produto (PR) é fornecido pelo usuário e o desconto é de 5%. Apresentar o valor do desconto.

---
+++ C10921_L1_Ex03.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj10921_L1_EX03
+{
+    internal class C10921_L1_EX03
+    {
+        static void Main(string[] args)
+        {
+
+            //Exercício 03.
+
+            //Cálculo de desconto onde o comprador informa o valor do produto.
+
+            #region
+
+            double PR, D;
+
+            Console.Write("Digite o preço do produto: R$");
+            PR = Convert.ToDouble(Console.ReadLine());
+
+            D = PR * 5 / 100;
+
+            Console.WriteLine("O valor do desconto é: R$" + D);
+
+            Console.ReadKey();
+
+            #endregion
+
+        }
+    }
+}
